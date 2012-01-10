c81086519df4ba1edb7d7c4aca803227802c35db
dyldragon <dyunlong@sina.com>
2012-01-10 16:34:15 +0800

    Signed-off-by: dyldragon <dyunlong@sina.com>

---
+++ src/cn/com/easyschool/Hello.java
@@ -0,0 +1,13 @@
+package cn.com.easyschool;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!!!");
+	}
+
+}
